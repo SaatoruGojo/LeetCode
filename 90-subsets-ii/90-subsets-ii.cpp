2c4a3f8bd5956b66125ffb4c62dd02410b2999c7
Harsh Anand <72392089+harshanand23561@users.noreply.github.com>
2022-09-04 02:01:01 +0530

    Time: 314 ms (5.31%), Space: 60.6 MB (5.14%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,42 +1,39 @@
-class Solution {
+class Solution
+{
 
-    private: void solve(vector<int> &nums,vector<int> output,int index,vector<vector<int>> &ans)
+    private: void solve(vector<int> &nums, vector<int> output, int index, vector< vector< int>> &ans)
     {
-        if(index>=nums.size())
+        if (index >= nums.size())
         {
-           
-            for(int i=0;i<ans.size();++i)
+
+            for (int i = 0; i < ans.size(); ++i)
             {
-                if(ans[i]==output)
+                if (ans[i] == output)
                     return;
-                vector<int> temp=ans[i];
-                reverse(temp.begin(),temp.end());
-                 if(temp==output)
+                vector<int> temp = ans[i];
+                reverse(temp.begin(), temp.end());
+                if (temp == output)
                     return;
             }
             ans.push_back(output);
             return;
         }
-        
-        solve(nums,output,index+1,ans);
-        
+
+        solve(nums, output, index + 1, ans);
+
         output.push_back(nums[index]);
-        solve(nums,output,index+1,ans);
-        
-        
+        solve(nums, output, index + 1, ans);
     }
-    
-    
+
     public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-        vector<vector<int>> ans;
-        vector<int> output;
-        int index=0;
-        sort(nums.begin(),nums.end());
-        solve(nums,output,index,ans);
-        return ans;
-    
-        
-    }
+        vector<vector < int>> subsetsWithDup(vector<int> &nums)
+        {
+
+            vector<vector < int>> ans;
+            vector<int> output;
+            int index = 0;
+            sort(nums.begin(), nums.end());
+            solve(nums, output, index, ans);
+            return ans;
+        }
 };
